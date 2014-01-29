1c38655330a92de38fdb08c7f48e9b5b809bc11c
clizby <clizby@localhost.westell.com>
2014-01-29 11:58:09 -0500

    remove comments

--- test.rb
+++ test.rb
@@ -1,33 +1,6 @@
 require 'cinch'
 require './plugins/autokick'
 
-#class MultiCommands
-#  include Cinch::Plugin
-#  listen_to :join
-#  match /autoop (on|off|)$/
-#  match /asshat (.+)/, method: :asshat
-#  match /command2 (.+)/, method: :command2
-#  match /^command3 (.+)/, use_prefix: false
-#
-#  def listen(m)
-#    @admins = ['gtsphere', 'drewlander', 'FU5T']
-#    unless @admins.include?(m.user.nick) 
-#       m.channel.kick(m.user)
-#    end
-#  end
-#  def asshat(m, arg)
-#    m.reply "#{arg} is an asshat"
-#  end
-#
-#  def command2(m, arg)
-#    m.reply "command2, arg: #{arg}"
-#  end
-#
-#  def execute(m, arg)
-#    m.reply "command3, arg: #{arg}"
-#  end
-#end
-
 bot = Cinch::Bot.new do
   configure do |c|
     c.nick            = "cinch_multi"
